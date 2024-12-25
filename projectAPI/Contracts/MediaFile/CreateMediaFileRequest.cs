eb50e208fe2b4f87eb7665a545bcaff51f7f9f2a
Hukkatir <dudinassonya@gmail.com>
2024-12-25 19:09:09 +0300

    upd media file

--- projectAPI/Contracts/MediaFile/CreateMediaFileRequest.cs
+++ projectAPI/Contracts/MediaFile/CreateMediaFileRequest.cs
@@ -2,6 +2,7 @@
 {
     public class CreateMediaFileRequest
     {
+        public int MediaFileId { get; set; }
         public int MediaId { get; set; }
         public int FileId { get; set; }
         public string MediaFileName { get; set; } = null!;
