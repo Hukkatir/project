e968eead8027e3c0f35de912d3fa1d5d8fca56eb
Hukkatir <dudinassonya@gmail.com>
2024-12-25 19:19:08 +0300

    upd try 2

--- projectAPI/Contracts/MediaFile/CreateMediaFileRequest.cs
+++ projectAPI/Contracts/MediaFile/CreateMediaFileRequest.cs
@@ -2,7 +2,6 @@
 {
     public class CreateMediaFileRequest
     {
-        public int MediaFileId { get; set; }
         public int MediaId { get; set; }
         public int FileId { get; set; }
         public string MediaFileName { get; set; } = null!;
